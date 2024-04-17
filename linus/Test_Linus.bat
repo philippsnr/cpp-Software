e582f9040bdfa05e170c0a41e138b3cc7def7f16
Nils897 <161706814+Nils897@users.noreply.github.com>
2024-04-17 16:31:54 +0200

    Update Test_Linus.bat

--- linus/Test_Linus.bat
+++ linus/Test_Linus.bat
@@ -1,3 +1,3 @@
 @echo on
-C:\Windows\System32\cmd.exe /k "C:\Test\batch1.bat && set TEST_VARIABLE=C:\Test && set path=C:\Test;E:\Test;%PATH% && ""C:\Programme(x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"" 
+C:\Windows\System32\cmd.exe /k "C:\Test\batch1.bat && set TEST_VARIABLE=C:\Test && set path=C:\Test;E:\Test;%PATH% && ""C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"" 
 @echo off
