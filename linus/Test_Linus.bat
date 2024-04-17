ef9ba3441c2e7c9088fcb0bd8734cddd15723ec6
Nils897 <161706814+Nils897@users.noreply.github.com>
2024-04-17 15:55:52 +0200

    Add files via upload

---
+++ linus/Test_Linus.bat
@@ -0,0 +1,3 @@
+@echo on
+C:\Windows\System32\cmd.exe /k "C:\Test\batch1.bat && set TEST_VARIABLE=C:\Test && set path=C:\Test;E:\Test;%PATH% && ""C:\Programme(x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"" 
+@echo off
